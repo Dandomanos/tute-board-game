4f9f253ac131c7a29db8cc56516d58fdb5e33ecc
JorgeL <jorge.lucas@gigigo.com>
2018-11-25 16:32:24 +0100

    New: Remove console logs

--- game/utils.js
+++ game/utils.js
@@ -1,13 +1,8 @@
 const RANKS = [ 2, 4, 5, 6, 7, 10, 11, 12, 3, 1]
 
 const getSuitCards = (suit, cards) => cards.filter(card => card.suit === suit)
-const getHigherCards = (rank, cards) => cards.filter(card => {
-    const indexOfCard = RANKS.indexOf(card.rank)
-
-    console.log("indexOfRank", rank)
-    console.log("Card rank", card.rank, indexOfCard)
-    return rank < indexOfCard
-})
+const getHigherCards = (rank, cards) =>
+  cards.filter(card => rank < RANKS.indexOf(card.rank))
 const getWinnerCard = cards => Math.max.apply( Math, cards.map(card => RANKS.indexOf(card.rank) ))
 
     
